ff0af1eeea6ab94364703b416535a17570a419b5
Marcio Antonio dos Santos <marcioasan@gmail.com>
2022-06-07 14:07:54 -0300

    13.10. Instalando a biblioteca de gráficos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+//13.10. Instalando a biblioteca de gráficos
+import { Col, Row, Typography } from 'antd';
+import CompanyMetrics from './app/layouts/Default/features/CompanyMetrics';
+
+const { Title, Paragraph } = Typography;
+
 function App() {
-  return <div>app</div>;
+  return (
+    <Row>
+      <Col span={24}>
+        <Title level={2}>Olá, José Sousa</Title>
+        <Paragraph>Este é o resumo da empresa nos últimos doze meses</Paragraph>
+      </Col>
+      <Col span={24}>
+        <CompanyMetrics />
+      </Col>
+    </Row>
+  );
 }
 
 export default App;
