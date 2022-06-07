8907e2c65fd4c6db3f674b526aff0fa7b3451dad
Marcio Antonio dos Santos <marcioasan@gmail.com>
2022-06-07 10:53:38 -0300

    13.9. Descentralizando o Layout

---
+++ src/app/layouts/Default/Breadcrumb.tsx
@@ -0,0 +1,12 @@
+//13.9. Descentralizando o Layout
+import { Breadcrumb as AntdBreadcrumb } from 'antd';
+
+export default function Breadcrumb() {
+  return (
+    <AntdBreadcrumb style={{ margin: '16px 0' }}>
+      <AntdBreadcrumb.Item>Home</AntdBreadcrumb.Item>
+      <AntdBreadcrumb.Item>List</AntdBreadcrumb.Item>
+      <AntdBreadcrumb.Item>App</AntdBreadcrumb.Item>
+    </AntdBreadcrumb>
+  );
+}
---
+++ src/app/layouts/Default/index.ts
@@ -0,0 +1,2 @@
+//13.9. Descentralizando o Layout
+export { default } from './Default.layout';
