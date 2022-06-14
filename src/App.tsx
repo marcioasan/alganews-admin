a1352ed7cc70cf82bd5f151d8f05d5001d845703
Marcio Antonio dos Santos <marcioasan@gmail.com>
2022-06-14 20:14:07 -0300

    13.19. Espaçamento entre Rows (componente Space

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 //13.10. Instalando a biblioteca de gráficos
-import { Col, Row, Typography } from 'antd';
+import { Col, Divider, Row, Space, Typography } from 'antd';
 import CompanyMetrics from './app/features/CompanyMetrics';
 import LatestPosts from './app/features/LatestPosts';
 
@@ -7,21 +7,27 @@ const { Title, Paragraph } = Typography;
 
 function App() {
   return (
-    <Row>
-      <Col span={24}>
-        <Title level={2}>Olá, José Sousa</Title>
-        <Paragraph>Este é o resumo da empresa nos últimos doze meses</Paragraph>
-      </Col>
-      <Col span={24}>
-        <CompanyMetrics />
-      </Col>
-      <Col span={24}>
-        <Title level={3}>Últimos posts</Title>
-      </Col>
-      <Col span={24}>
-        <LatestPosts />
-      </Col>
-    </Row>
+    /* 13.19. Espaçamento entre Rows (componente Space */
+    <Space direction='vertical' size={'middle'}>
+      <Row>
+        <Col span={24}>
+          <Title level={2}>Olá, José Sousa</Title>
+          <Paragraph>Este é o resumo da empresa nos últimos doze meses</Paragraph>
+        </Col>
+        <Col span={24}>
+          <CompanyMetrics />
+        </Col>
+      </Row>
+      <Divider />
+      <Row>
+        <Col span={24}>
+          <Title level={3}>Últimos posts</Title>
+        </Col>
+        <Col span={24}>
+          <LatestPosts />
+        </Col>
+      </Row>
+    </Space>
   );
 }
 
