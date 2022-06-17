d84467203ccf88cbae09c38d97f637403db1ce85
Marcio Antonio dos Santos <marcioasan@gmail.com>
2022-06-17 12:44:23 -0300

    13.28. Adicionando a logo no header

--- src/app/layouts/Default/Header.tsx
+++ src/app/layouts/Default/Header.tsx
@@ -1,17 +1,18 @@
 //13.9. Descentralizando o Layout
-import { Menu, Layout } from 'antd';
+import { Layout, Row } from 'antd';
+import Avatar from 'antd/lib/avatar/avatar';
+import logo from '../../../assets/logo.svg';
 
 const { Header } = Layout;
 
 export default function DefaultLayoutHeader() {
   return (
+    /* 13.28. Adicionando a logo no header */
     <Header className='header'>
-      <div className='logo' />
-      <Menu theme='dark' mode='horizontal' defaultSelectedKeys={['2']}>
-        <Menu.Item key='1'>nav 1</Menu.Item>
-        <Menu.Item key='2'>nav 2</Menu.Item>
-        <Menu.Item key='3'>nav 3</Menu.Item>
-      </Menu>
+      <Row justify='space-between' style={{ height: '100%' }} align='middle'>
+        <img src={logo} alt={'AlgaNews Admin'} />
+        <Avatar />
+      </Row>
     </Header>
   );
 }
