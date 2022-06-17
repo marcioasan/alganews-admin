80f269028a637ef105b73709a1b02b6d1eab2313
Marcio Antonio dos Santos <marcioasan@gmail.com>
2022-06-17 16:12:29 -0300

    13.29. Configurando o CRACO no projeto

---
+++ craco.config.js
@@ -0,0 +1,17 @@
+//13.29. Configurando o CRACO no projeto - 4'35"
+const CracoLessPlugin = require('craco-less');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
