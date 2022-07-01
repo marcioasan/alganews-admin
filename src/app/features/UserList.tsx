62501551fc131594d323906c7ef1fd4a0b5872fa
Marcio Antonio dos Santos <marcioasan@gmail.com>
2022-07-01 20:45:13 -0300

    14.5. Definindo uma largura para a coluna

--- src/app/features/UserList.tsx
+++ src/app/features/UserList.tsx
@@ -21,6 +21,7 @@ export default function UserList() {
           {
             dataIndex: 'name',
             title: 'Nome',
+            width: 160,
             //14.3. Recuperando dados além da coluna
             render(name: string, row) {
               return (
@@ -37,6 +38,7 @@ export default function UserList() {
             dataIndex: 'email',
             title: 'Email',
             ellipsis: true,
+            width: 240,
           },
           {
             dataIndex: 'role',
