31423c489eeaa7a863566d0b11a67a059abad96d
Marcio Antonio dos Santos <marcioasan@gmail.com>
2022-07-14 21:34:35 -0300

    14.11. Filtro de busca personalizado e reutilizável

--- src/app/features/UserList.tsx
+++ src/app/features/UserList.tsx
@@ -1,10 +1,11 @@
 //14.1. Tabela de usuários
-import { Avatar, Button, Space, Switch, Table, Tag, Typography } from 'antd';
+import { Avatar, Button, Card, Input, Space, Switch, Table, Tag, Typography } from 'antd';
 import { format } from 'date-fns';
 import { User } from 'marcioasan-sdk';
 import { useEffect } from 'react';
 import useUsers from '../../core/hooks/useUsers';
-import { EyeOutlined, EditOutlined } from '@ant-design/icons';
+import { EyeOutlined, EditOutlined, SearchOutlined } from '@ant-design/icons';
+import { ColumnProps } from 'antd/lib/table';
 
 export default function UserList() {
   const { users, fetchUsers, toggleUserStatus } = useUsers();
@@ -12,6 +13,70 @@ export default function UserList() {
   useEffect(() => {
     fetchUsers();
   }, [fetchUsers]);
+
+  //14.11. Filtro de busca personalizado e reutilizável - 3' INICIO
+  const handleReset = (clearFilters: () => void) => {
+    clearFilters();
+  };
+
+  const getColumnSearchProps = (
+    dataIndex: keyof User.Summary,
+    displayName?: string
+  ): ColumnProps<User.Summary> => ({
+    filterDropdown: ({ selectedKeys, setSelectedKeys, confirm, clearFilters }) => (
+      <Card>
+        <Input
+          style={{ marginBottom: 8, display: 'block' }}
+          value={selectedKeys[0]}
+          placeholder={`Buscar ${displayName || dataIndex}`}
+          onChange={(e) => {
+            setSelectedKeys(e.target.value ? [e.target.value] : []);
+          }}
+          onPressEnter={() => confirm()}
+        />
+        <Space>
+          <Button
+            type={'primary'}
+            size={'small'}
+            style={{ width: 90 }}
+            onClick={() => confirm()}
+            icon={<SearchOutlined />}
+          >
+            Buscar
+          </Button>
+          {/* https://app.algaworks.com/forum/topicos/86696/botao-limpar-nao-esta-funcionando - A forma como foi feita na aula não funciona mais, essa foi a solução de um aluno*/}
+          <Button
+            size={'small'}
+            style={{ width: 90 }}
+            onClick={() => {
+              clearFilters && handleReset(clearFilters);
+              confirm();
+            }}
+          >
+            Limpar
+          </Button>
+          {/* 
+          <Button onClick={clearFilters} size={'small'} style={{ width: 90 }}>
+            Limpar
+          </Button>
+           */}
+        </Space>
+      </Card>
+    ),
+    filterIcon: (filtered: boolean) => (
+      <SearchOutlined style={{ color: filtered ? '#0099ff' : undefined }} />
+    ),
+    //14.11. Filtro de busca personalizado e reutilizável - 17'
+    // @ts-ignore
+    onFilter: (value, record) =>
+      record[dataIndex]
+        ? record[dataIndex]
+            .toString()
+            .toLowerCase()
+            .includes((value as string).toLowerCase())
+        : '',
+  });
+  //14.11. Filtro de busca personalizado e reutilizável - 3' FIM
 
   return (
     <>
@@ -22,6 +87,7 @@ export default function UserList() {
             dataIndex: 'name',
             title: 'Nome',
             width: 160,
+            ...getColumnSearchProps('name', 'Nome'),
             //14.3. Recuperando dados além da coluna
             render(name: string, row) {
               return (
@@ -39,6 +105,7 @@ export default function UserList() {
             title: 'Email',
             ellipsis: true,
             width: 240,
+            ...getColumnSearchProps('email', 'Email'),
           },
           {
             dataIndex: 'role',
